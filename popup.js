b68ae704d1afa07f3dfbc38e974bb17520cc77ae
LinusGordon <linusgordon@gmail.com>
2018-03-02 23:10:51 -0500

    Add persistent tiers

--- popup.js
+++ popup.js
@@ -1,3 +1,31 @@
 document.addEventListener('DOMContentLoaded', function() {
-    alert("Hello World");
+    
+    chrome.storage.sync.get("tier", function (obj) { 
+                                        var tier;
+                                        if (obj == undefined) {
+                                            chrome.storage.sync.set({ "tier" : "platinum" }, function () {});
+                                            tier = "platinum";
+                                        } else {
+                                            tier = obj["tier"]
+                                        }
+                                        document.getElementById(tier).checked = true;
+                                    });
+    setTier();
+
 });
+    
+function setTier () {
+    
+    var rad = document.tierSystem.tier;
+    var prev = null;
+    for(var i = 0; i < rad.length; i++) {
+        rad[i].onclick = function() {
+            if(this !== prev) {
+                prev = this;
+            }
+            tier = (this.value)
+            chrome.storage.sync.set({ "tier" : tier }, function () {});
+
+        };
+    }
+}
