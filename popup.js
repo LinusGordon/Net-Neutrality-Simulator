2351c5eb2ee3fb4ff9f215ae48d4bcc6d4ea84fe
LinusGordon <linusgordon@gmail.com>
2018-03-02 23:21:44 -0500

    Add outline of function to handle tiers

--- popup.js
+++ popup.js
@@ -1,5 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
     
+    getTier(tierHandler);
+    setTier();
+});
+    
+/* 
+ * Get the tier and select it on the radio buttons.
+ * If it doesn't exist yet (first time user) then make it platinum
+ */
+function getTier (callback) {
+
     chrome.storage.sync.get("tier", function (obj) { 
                                         var tier;
                                         if (obj == undefined) {
@@ -9,11 +19,14 @@ document.addEventListener('DOMContentLoaded', function() {
                                             tier = obj["tier"]
                                         }
                                         document.getElementById(tier).checked = true;
+                                        callback(tier);
                                     });
-    setTier();
+}
 
-});
-    
+/* 
+ * Sets the tier based off of the user input and stores it in sync storage 
+ * so that is persists throughout sessions.
+ */
 function setTier () {
     
     var rad = document.tierSystem.tier;
@@ -28,4 +41,8 @@ function setTier () {
 
         };
     }
+}
+
+function tierHandler (tier) {
+    console.log(tier);
 }
